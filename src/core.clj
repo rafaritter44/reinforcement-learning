460e3e01164dfc24acc3deb257fa09ef68c9a4f8
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-05 15:06:14 -0300

    Implement tic-tac-toe

--- src/core.clj
+++ src/core.clj
@@ -1,1 +1,46 @@
 (ns core)
+
+(def empty-board
+  (vec (repeat 9 nil)))
+
+(def winning-lines
+  [[0 1 2]
+   [3 4 5]
+   [6 7 8]
+   [0 3 6]
+   [1 4 7]
+   [2 5 8]
+   [0 4 8]
+   [2 4 6]])
+
+(defn winner [board]
+  (some
+   (fn [[a b c]]
+     (let [mark (board a)]
+       (when (and mark
+                  (= mark (board b))
+                  (= mark (board c)))
+         mark)))
+   winning-lines))
+
+(defn full? [board]
+  (every? some? board))
+
+(defn terminal? [board]
+  (or (winner board)
+      (full? board)))
+
+(defn empty-squares [board]
+  (keep-indexed
+   (fn [index square]
+     (when (nil? square)
+       index))
+   board))
+
+(defn make-move [board square player]
+  (assoc board square player))
+
+(defn successors [board player]
+  (mapv
+   #(make-move board % player)
+   (empty-squares board)))
